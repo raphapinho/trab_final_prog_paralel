73dcbb0bd40772e428504a78f1e7dd3ac51e0087
Raphael Pinho <73498424+raphapinho@users.noreply.github.com>
2023-06-19 22:50:22 -0300

    adcionado eficiencia, speedup e desempenho.

--- collatz_seq_parel.c
+++ collatz_seq_parel.c
@@ -1,12 +1,47 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <omp.h> //usado somente para pegar o tempo de execução
 
 #define N 100000
 #define MAX_ITERACOES 1000
 
-int main(void) {
+double collts_seq(void) {
+  int convergiu[N] = {0};
+
+  double start_time = omp_get_wtime();
+
+#pragma omp parallel for
+  for (int i = 0; i < N; i++) {
+    int numTeste = i + 1;
+    int iters = 0;
+
+    do {
+      iters++;
+      numTeste = (numTeste % 2 == 1) ? (numTeste * 3) + 1 : numTeste / 2;
+
+      if (numTeste == 4) {
+        convergiu[i] = 1;
+      }
+
+    } while (numTeste != 4 && iters < MAX_ITERACOES);
+  }
+
+  double end_time = omp_get_wtime();
+  double elapsed_time = end_time - start_time;
+
+/*
+  for (int i = 0; i < N; i++) {
+    printf("Número %d %s.\n", i + 1,
+           convergiu[i] ? "convergiu" : "não convergiu");
+  }
+*/
+
+  printf("Tempo de execucao: %.6f segundos\n", elapsed_time);
+
+  return elapsed_time;
+}
+
+double collatz_paral(void) {
 
     double start_time = omp_get_wtime();
 
@@ -54,9 +89,25 @@ int main(void) {
 
     printf("Tempo de execucao: %.6f segundos\n", elapsed_time);
 
-  return 0;
+  return elapsed_time;
 
   //Speedup = Tseq / Tpar
-  //Eficiência = (Speedup / N) * 100%
+  //Eficiência = (Speedup / N) * 100%  #N é o número de processadores ou núcleos utilizados.
   //Desempenho = Trabalho Total / Tempo Total
+}
+
+int main(void){
+
+    double time_seq = collts_seq();
+    double time_paralel = collatz_paral();
+
+    double speedup = time_seq /time_paralel;
+
+    double eficiencia = (speedup/6) * 100;
+
+    printf("o speedup é de %3.4f \n", speedup);
+
+    printf("A eficiencia é de %3.4f \n", eficiencia);
+
+    return 0;
 }
